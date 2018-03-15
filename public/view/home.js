2e0a35faadfe8c7fc19f51f670c6161ae6bad25d
VladGaryachiy <32650566+VladGaryachiy@users.noreply.github.com>
2018-03-15 21:52:15 +0200

    c_show

--- public/view/home.js
+++ public/view/home.js
@@ -2,11 +2,21 @@ let React = require('react');
 let ReactDOM = require('react-dom');
 let Carousel = require('./component/carousel').Carousel;
 
+let Tween = require('rc-tween-one/lib/TweenOne');
+let ScrollOverPack = require('rc-scroll-anim/lib/ScrollOverPack');
+let Link = require('react-router-dom').Link;
+
 let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
+
+        this.UpScroll = this.UpScroll.bind(this);
+    }
+
+    UpScroll() {
+        window.scrollTo(0, 0);
     }
 
     render() {
@@ -31,24 +41,128 @@ class Home extends React.Component {
                         React.createElement(
                             'h1',
                             { className: 'about-company' },
-                            '\u041F\u0440\u043E \u043D\u0430\u0441'
+                            React.createElement(
+                                'p',
+                                { className: 'side-line' },
+                                '   '
+                            ),
+                            '  \u041F\u0440\u043E \u043D\u0430\u0441  ',
+                            React.createElement(
+                                'p',
+                                { className: 'side-line' },
+                                '   '
+                            )
                         ),
                         React.createElement(
-                            'div',
-                            { className: 'row history-company' },
+                            ScrollOverPack,
+                            null,
                             React.createElement(
-                                'div',
-                                { className: 'col-md-6' },
+                                Tween,
+                                {
+                                    key: '1',
+                                    animation: { y: 50, type: 'from', ease: 'easeOutQuart', opacity: 0 },
+                                    reverseDelay: 200
+                                },
                                 React.createElement(
-                                    'p',
-                                    { className: 'about-company-text' },
-                                    'GMC - \u0446\u0435 \u043D\u0430\u0439\u0431\u0456\u043B\u044C\u0448\u0430 \u0430\u043C\u0435\u0440\u0438\u043A\u0430\u043D\u0441\u044C\u043A\u0430 \u0430\u0432\u0442\u043E\u043C\u043E\u0431\u0456\u043B\u044C\u043D\u0430 \u043A\u043E\u0440\u043F\u043E\u0440\u0430\u0446\u0456\u044F, \u0434\u043E 2008 \u0440\u043E\u043A\u0443 \u043F\u0440\u043E\u0442\u044F\u0433\u043E\u043C 77 \u0440\u043E\u043A\u0456\u0432 \u043D\u0430\u0439\u0431\u0456\u043B\u044C\u0448\u0438\u0439 \u0432\u0438\u0440\u043E\u0431\u043D\u0438\u043A \u0430\u0432\u0442\u043E\u043C\u043E\u0431\u0456\u043B\u0456\u0432 \u0432 \u0441\u0432\u0456\u0442\u0456. \u0417\u0430 \u0440\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442\u0430\u043C\u0438 2014 \u043A\u043E\u043D\u0446\u0435\u0440\u043D \u0437\u0430\u0439\u043C\u0430\u0454 \u0442\u0440\u0435\u0442\u0454 \u043C\u0456\u0441\u0446\u0435 \u0432 \u0441\u0432\u0456\u0442\u0456 (\u043F\u0456\u0441\u043B\u044F Toyota \u0456 Volkswagen) \u0437\u0430 \u043A\u0456\u043B\u044C\u043A\u0456\u0441\u0442\u044E \u043F\u0440\u043E\u0434\u0430\u043D\u0438\u0445 \u0430\u0432\u0442\u043E\u043C\u043E\u0431\u0456\u043B\u0456\u0432 (9,92 \u043C\u0456\u043B\u044C\u0439\u043E\u043D\u0430 \u0430\u0432\u0442\u043E). \u0412\u0438\u0440\u043E\u0431\u043D\u0438\u0446\u0442\u0432\u043E \u043D\u0430\u043B\u0430\u0433\u043E\u0434\u0436\u0435\u043D\u0435 \u0432 35 \u043A\u0440\u0430\u0457\u043D\u0430\u0445, \u043F\u0440\u043E\u0434\u0430\u0436 \u0432 192 \u043A\u0440\u0430\u0457\u043D\u0430\u0445. \u0428\u0442\u0430\u0431-\u043A\u0432\u0430\u0440\u0442\u0438\u0440\u0430 \u043A\u043E\u043C\u043F\u0430\u043D\u0456\u0457 \u0440\u043E\u0437\u0442\u0430\u0448\u043E\u0432\u0430\u043D\u0430 \u0432 \u0414\u0435\u0442\u0440\u043E\u0439\u0442\u0456. \u041D\u0430 \u043F\u0435\u0432\u043D\u0438\u0445 \u0435\u0442\u0430\u043F\u0430\u0445 \u0441\u0432\u043E\u0433\u043E \u0456\u0441\u043D\u0443\u0432\u0430\u043D\u043D\u044F \u0432\u0445\u043E\u0434\u0438\u043B\u0430 \u0434\u043E \u0434\u0432\u0430\u0434\u0446\u044F\u0442\u043A\u0438 \u043D\u0430\u0439\u0431\u0456\u043B\u044C\u0448\u0438\u0445 \u043A\u043E\u043C\u043F\u0430\u043D\u0456\u0439-\u043F\u0456\u0434\u0440\u044F\u0434\u043D\u0438\u043A\u0456\u0432 \u0412\u041F\u041A \u0421\u0428\u0410. \u0411\u043B\u0438\u0437\u044C\u043A\u043E \u043F\'\u044F\u0442\u043E\u0457 \u0447\u0430\u0441\u0442\u0438\u043D\u0438 \u0434\u043E\u0445\u043E\u0434\u0456\u0432 \u0432\u0456\u0434 \u043F\u0440\u043E\u0434\u0430\u0436\u0456\u0432 \u043F\u0440\u043E\u0434\u0443\u043A\u0446\u0456\u0457 \u0456 \u043F\u043E\u0441\u043B\u0443\u0433, \u0449\u043E \u043D\u0430\u0434\u0430\u044E\u0442\u044C\u0441\u044F \u0441\u0442\u0430\u043D\u043E\u0432\u0438\u0442\u044C \u0444\u0435\u0434\u0435\u0440\u0430\u043B\u044C\u043D\u0438\u0439 \u043A\u043B\u0456\u0454\u043D\u0442\u0441\u044C\u043A\u0438\u0439 \u0441\u0435\u043A\u0442\u043E\u0440 \u043E\u0431\u0441\u043B\u0443\u0433\u043E\u0432\u0443\u0432\u0430\u043D\u043D\u044F \u0432\u0456\u0439\u0441\u044C\u043A\u043E\u0432\u0438\u0445 \u0437\u0430\u043C\u043E\u0432\u043B\u0435\u043D\u044C (\u0431\u0435\u0437 \u0443\u0440\u0430\u0445\u0443\u0432\u0430\u043D\u043D\u044F \u0456\u043D\u043E\u0437\u0435\u043C\u043D\u0438\u0445 \u0437\u0430\u043C\u043E\u0432\u043D\u0438\u043A\u0456\u0432 \u0430\u043C\u0435\u0440\u0438\u043A\u0430\u043D\u0441\u044C\u043A\u043E\u0433\u043E \u043E\u0437\u0431\u0440\u043E\u0454\u043D\u043D\u044F \u0456 \u0432\u0456\u0439\u0441\u044C\u043A\u043E\u0432\u043E\u0457 \u0442\u0435\u0445\u043D\u0456\u043A\u0438).'
+                                    'div',
+                                    { className: 'row history-company' },
+                                    React.createElement(
+                                        'div',
+                                        { className: 'col-md-6 about-company-text-container' },
+                                        React.createElement(
+                                            'div',
+                                            { className: 'banner-home' },
+                                            React.createElement(
+                                                'p',
+                                                { className: 'about-company-text' },
+                                                React.createElement(
+                                                    'span',
+                                                    { className: 'logo-home' },
+                                                    'GMC'
+                                                ),
+                                                ' - \u0446\u0435 \u043D\u0430\u0439\u0431\u0456\u043B\u044C\u0448\u0430 \u0430\u043C\u0435\u0440\u0438\u043A\u0430\u043D\u0441\u044C\u043A\u0430 \u0430\u0432\u0442\u043E\u043C\u043E\u0431\u0456\u043B\u044C\u043D\u0430 \u043A\u043E\u0440\u043F\u043E\u0440\u0430\u0446\u0456\u044F, \u0434\u043E 2008 \u0440\u043E\u043A\u0443 \u043F\u0440\u043E\u0442\u044F\u0433\u043E\u043C 77 \u0440\u043E\u043A\u0456\u0432 \u043D\u0430\u0439\u0431\u0456\u043B\u044C\u0448\u0438\u0439 \u0432\u0438\u0440\u043E\u0431\u043D\u0438\u043A \u0430\u0432\u0442\u043E\u043C\u043E\u0431\u0456\u043B\u0456\u0432 \u0432 \u0441\u0432\u0456\u0442\u0456. \u0417\u0430 \u0440\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442\u0430\u043C\u0438 2014 \u043A\u043E\u043D\u0446\u0435\u0440\u043D \u0437\u0430\u0439\u043C\u0430\u0454 \u0442\u0440\u0435\u0442\u0454 \u043C\u0456\u0441\u0446\u0435 \u0432 \u0441\u0432\u0456\u0442\u0456 (\u043F\u0456\u0441\u043B\u044F Toyota \u0456 Volkswagen) \u0437\u0430 \u043A\u0456\u043B\u044C\u043A\u0456\u0441\u0442\u044E \u043F\u0440\u043E\u0434\u0430\u043D\u0438\u0445 \u0430\u0432\u0442\u043E\u043C\u043E\u0431\u0456\u043B\u0456\u0432 (9,92 \u043C\u0456\u043B\u044C\u0439\u043E\u043D\u0430 \u0430\u0432\u0442\u043E). \u0412\u0438\u0440\u043E\u0431\u043D\u0438\u0446\u0442\u0432\u043E \u043D\u0430\u043B\u0430\u0433\u043E\u0434\u0436\u0435\u043D\u0435 \u0432 35 \u043A\u0440\u0430\u0457\u043D\u0430\u0445, \u043F\u0440\u043E\u0434\u0430\u0436 \u0432 192 \u043A\u0440\u0430\u0457\u043D\u0430\u0445. \u0428\u0442\u0430\u0431-\u043A\u0432\u0430\u0440\u0442\u0438\u0440\u0430 \u043A\u043E\u043C\u043F\u0430\u043D\u0456\u0457 \u0440\u043E\u0437\u0442\u0430\u0448\u043E\u0432\u0430\u043D\u0430 \u0432 \u0414\u0435\u0442\u0440\u043E\u0439\u0442\u0456. \u041D\u0430 \u043F\u0435\u0432\u043D\u0438\u0445 \u0435\u0442\u0430\u043F\u0430\u0445 \u0441\u0432\u043E\u0433\u043E \u0456\u0441\u043D\u0443\u0432\u0430\u043D\u043D\u044F \u0432\u0445\u043E\u0434\u0438\u043B\u0430 \u0434\u043E \u0434\u0432\u0430\u0434\u0446\u044F\u0442\u043A\u0438 \u043D\u0430\u0439\u0431\u0456\u043B\u044C\u0448\u0438\u0445 \u043A\u043E\u043C\u043F\u0430\u043D\u0456\u0439-\u043F\u0456\u0434\u0440\u044F\u0434\u043D\u0438\u043A\u0456\u0432 \u0412\u041F\u041A \u0421\u0428\u0410. \u0411\u043B\u0438\u0437\u044C\u043A\u043E \u043F\'\u044F\u0442\u043E\u0457 \u0447\u0430\u0441\u0442\u0438\u043D\u0438 \u0434\u043E\u0445\u043E\u0434\u0456\u0432 \u0432\u0456\u0434 \u043F\u0440\u043E\u0434\u0430\u0436\u0456\u0432 \u043F\u0440\u043E\u0434\u0443\u043A\u0446\u0456\u0457 \u0456 \u043F\u043E\u0441\u043B\u0443\u0433, \u0449\u043E \u043D\u0430\u0434\u0430\u044E\u0442\u044C\u0441\u044F \u0441\u0442\u0430\u043D\u043E\u0432\u0438\u0442\u044C \u0444\u0435\u0434\u0435\u0440\u0430\u043B\u044C\u043D\u0438\u0439 \u043A\u043B\u0456\u0454\u043D\u0442\u0441\u044C\u043A\u0438\u0439 \u0441\u0435\u043A\u0442\u043E\u0440 \u043E\u0431\u0441\u043B\u0443\u0433\u043E\u0432\u0443\u0432\u0430\u043D\u043D\u044F \u0432\u0456\u0439\u0441\u044C\u043A\u043E\u0432\u0438\u0445 \u0437\u0430\u043C\u043E\u0432\u043B\u0435\u043D\u044C (\u0431\u0435\u0437 \u0443\u0440\u0430\u0445\u0443\u0432\u0430\u043D\u043D\u044F \u0456\u043D\u043E\u0437\u0435\u043C\u043D\u0438\u0445 \u0437\u0430\u043C\u043E\u0432\u043D\u0438\u043A\u0456\u0432 \u0430\u043C\u0435\u0440\u0438\u043A\u0430\u043D\u0441\u044C\u043A\u043E\u0433\u043E \u043E\u0437\u0431\u0440\u043E\u0454\u043D\u043D\u044F \u0456 \u0432\u0456\u0439\u0441\u044C\u043A\u043E\u0432\u043E\u0457 \u0442\u0435\u0445\u043D\u0456\u043A\u0438).'
+                                            )
+                                        )
+                                    ),
+                                    React.createElement('div', { className: 'col-md-6 about-company-img-container' })
                                 )
-                            ),
+                            )
+                        ),
+                        React.createElement(
+                            ScrollOverPack,
+                            null,
                             React.createElement(
-                                'div',
-                                { className: 'col-md-6' },
-                                React.createElement('img', { src: '../img/gmc1.jpg', height: '460px', width: '600px', alt: '' })
+                                Tween,
+                                {
+                                    key: '2',
+                                    animation: { y: 50, type: 'from', ease: 'easeOutQuart', opacity: 0 },
+                                    reverseDelay: 200
+                                },
+                                React.createElement(
+                                    'div',
+                                    { className: 'col-md-12' },
+                                    React.createElement(
+                                        'h1',
+                                        { className: 'about-company' },
+                                        React.createElement(
+                                            'p',
+                                            { className: 'side-line' },
+                                            '   '
+                                        ),
+                                        '  \u0422\u0438\u043F\u0438 \u0430\u0432\u0442\u043E\u043C\u043E\u0431\u0456\u043B\u0456\u0432  ',
+                                        React.createElement(
+                                            'p',
+                                            { className: 'side-line' },
+                                            '   '
+                                        )
+                                    ),
+                                    React.createElement(
+                                        'div',
+                                        { className: 'row home-type-cars-container' },
+                                        React.createElement(
+                                            'div',
+                                            { className: 'col-md-4' },
+                                            React.createElement(
+                                                'h3',
+                                                { className: 'home-type-cars-name' },
+                                                '\u041F\u0456\u043A\u0430\u043F\u0438'
+                                            ),
+                                            React.createElement(
+                                                Link,
+                                                { to: '/pickups', onClick: this.UpScroll },
+                                                React.createElement('img', { src: '../img/home/type1.jpg', className: 'home-type-img', alt: '' })
+                                            )
+                                        ),
+                                        React.createElement(
+                                            'div',
+                                            { className: 'col-md-4' },
+                                            React.createElement(
+                                                'h3',
+                                                { className: 'home-type-cars-name' },
+                                                '\u041F\u043E\u0437\u0430\u0448\u043B\u044F\u0445\u043E\u0432\u0438\u043A\u0438'
+                                            ),
+                                            React.createElement(
+                                                Link,
+                                                { to: '/suvs', onClick: this.UpScroll },
+                                                React.createElement('img', { src: '../img/home/type2.jpg', className: 'home-type-img', alt: '' })
+                                            )
+                                        ),
+                                        React.createElement(
+                                            'div',
+                                            { className: 'col-md-4' },
+                                            React.createElement(
+                                                'h3',
+                                                { className: 'home-type-cars-name' },
+                                                '\u0424\u0443\u0440\u0433\u043E\u043D\u0438'
+                                            ),
+                                            React.createElement(
+                                                Link,
+                                                { to: '/vans', onClick: this.UpScroll },
+                                                React.createElement('img', { src: '../img/home/type3.jpg', className: 'home-type-img', alt: '' })
+                                            )
+                                        )
+                                    )
+                                )
                             )
                         )
                     )
--- public/view/home.jsx
+++ public/view/home.jsx
@@ -2,7 +2,9 @@ let React = require('react');
 let ReactDOM = require('react-dom');
 let Carousel = require('./component/carousel').Carousel;
 
-
+let Tween = require('rc-tween-one/lib/TweenOne');
+let ScrollOverPack = require('rc-scroll-anim/lib/ScrollOverPack');
+let Link = require('react-router-dom').Link;
 
 let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
@@ -10,6 +12,12 @@ let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
         class Home extends React.Component{
             constructor(props){
                 super(props);
+
+                this.UpScroll = this.UpScroll.bind(this);
+            }
+
+            UpScroll(){
+                window.scrollTo(0, 0);
             }
 
             render() {
@@ -24,26 +32,75 @@ let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
                              </div>
                              <div className="col-md-12 col-sm-12 col-xs-12 about-company-container " >
                                 <h1 className="about-company">
-                                    Про нас
+                                   <p className="side-line">   </p>  Про нас  <p className="side-line">   </p>
                                 </h1>
 
-                                 <div className="row history-company">
-                                     <div className="col-md-6">
-                                        <p className="about-company-text">
-                                            GMC - це найбільша американська автомобільна корпорація, до 2008 року протягом 77 років
-                                            найбільший виробник автомобілів в світі. За результатами 2014 концерн займає третє місце
-                                            в світі (після Toyota і Volkswagen) за кількістю проданих автомобілів (9,92 мільйона авто).
-                                            Виробництво налагоджене в 35 країнах, продаж в 192 країнах. Штаб-квартира компанії розташована
-                                            в Детройті. На певних етапах свого існування входила до двадцятки найбільших компаній-підрядників
-                                            ВПК США. Близько п'ятої частини доходів від продажів продукції і послуг, що надаються становить
-                                            федеральний клієнтський сектор обслуговування військових замовлень (без урахування іноземних
-                                            замовників американського озброєння і військової техніки).
-                                        </p>
-                                     </div>
-                                     <div className="col-md-6">
-                                         <img src="../img/gmc1.jpg" height="460px" width="600px" alt=""/>
-                                     </div>
-                                 </div>
+                                <ScrollOverPack>
+                                    <Tween
+                                        key="1"
+                                        animation={{ y: 50, type: 'from', ease: 'easeOutQuart', opacity: 0  }}
+                                        reverseDelay={200}
+                                    >
+                                        <div className="row history-company">
+                                            <div className="col-md-6 about-company-text-container">
+                                                <div className="banner-home">
+                                                    <p className="about-company-text">
+                                                        <span className="logo-home">GMC</span> - це найбільша американська автомобільна корпорація, до 2008 року протягом 77 років
+                                                        найбільший виробник автомобілів в світі. За результатами 2014 концерн займає третє місце
+                                                        в світі (після Toyota і Volkswagen) за кількістю проданих автомобілів (9,92 мільйона авто).
+                                                        Виробництво налагоджене в 35 країнах, продаж в 192 країнах. Штаб-квартира компанії розташована
+                                                        в Детройті. На певних етапах свого існування входила до двадцятки найбільших компаній-підрядників
+                                                        ВПК США. Близько п'ятої частини доходів від продажів продукції і послуг, що надаються становить
+                                                        федеральний клієнтський сектор обслуговування військових замовлень (без урахування іноземних
+                                                        замовників американського озброєння і військової техніки).
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div className="col-md-6 about-company-img-container" >
+
+                                            </div>
+                                        </div>
+                                    </Tween>
+                                </ScrollOverPack>
+                                 <ScrollOverPack>
+                                     <Tween
+                                         key="2"
+                                         animation={{ y: 50, type: 'from', ease: 'easeOutQuart', opacity: 0  }}
+                                         reverseDelay={200}
+                                     >
+                                         <div className="col-md-12">
+                                             <h1 className="about-company">
+                                                 <p className="side-line">   </p>  Типи автомобілів  <p className="side-line">   </p>
+                                             </h1>
+                                             <div className="row home-type-cars-container">
+                                                 <div className="col-md-4">
+                                                     <h3 className="home-type-cars-name">
+                                                         Пікапи
+                                                     </h3>
+                                                     <Link to="/pickups" onClick={this.UpScroll}>
+                                                         <img src="../img/home/type1.jpg" className="home-type-img" alt=""/>
+                                                     </Link>
+                                                 </div>
+                                                 <div className="col-md-4">
+                                                     <h3 className="home-type-cars-name">
+                                                         Позашляховики
+                                                     </h3>
+                                                     <Link to="/suvs" onClick={this.UpScroll}>
+                                                         <img src="../img/home/type2.jpg" className="home-type-img" alt=""/>
+                                                     </Link>
+                                                 </div>
+                                                 <div className="col-md-4">
+                                                     <h3 className="home-type-cars-name">
+                                                         Фургони
+                                                     </h3>
+                                                     <Link to="/vans" onClick={this.UpScroll}>
+                                                         <img src="../img/home/type3.jpg" className="home-type-img" alt=""/>
+                                                     </Link>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                     </Tween>
+                                 </ScrollOverPack>
                              </div>
                          </div>
                      </div>
